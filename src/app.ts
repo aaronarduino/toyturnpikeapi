f082cd3ed5e86741f47e8f686f0bef39a112a17b
Aaron Alexander <aarona.arduino@gmail.com>
2026-06-30 12:07:08 -0500

    feat: update routes and add new routes

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,22 @@
 // const express = require("express");
 // const express = require("express");
 import express from "express";
-import { toNodeHandler } from "better-auth/node";
+import { fromNodeHeaders, toNodeHandler } from "better-auth/node";
 import { auth, requireAuth } from "./utils/auth.js";
+import { client, db, testDb } from "./utils/database.js";
 var app = express();
 const port = 3000;
 
 app.all("/api/auth/{*any}", toNodeHandler(auth));
 app.use(express.json());
 
-app.get("/", requireAuth, (req, res) => {
-  res.send({ message: "Hello, world!" });
+app.get("/", async (req, res) => {
+  res.send({ message: "hello, world!" });
 });
+
+///////////////////////////////////////////////////////////////////////////////
+// Dashboard Routes ///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 
 app.get("/dashboard", requireAuth, (req, res) => {
   res.send({
@@ -28,23 +33,45 @@ app.get("/dashboard/payments", requireAuth, (req, res) => {
   res.send({
     statement_balance: 0.0,
     auto_pay_date: "07/01/2026",
-    current_accont_balance: 0.0,
+    current_account_balance: 0.0,
   });
 });
 
-app.get("/dashboard/payment_methods", requireAuth, (req, res) => {
+app.get("/dashboard/payment_methods", requireAuth, async (req, res) => {
+  const session = await auth.api.getSession({
+    headers: fromNodeHeaders(req.headers),
+  });
+  const paymentMethodsDb = testDb.collection("payment_methods");
+  const paymentMethod = await paymentMethodsDb.findOne({
+    account_id: session?.user.id,
+    primary: true,
+  });
+
   res.send({
     primary: {
-      card_number: 1234,
-      expiration_date: "07/01/2026",
+      card_number: paymentMethod?.card_last_four,
+      expiration_date: paymentMethod?.expiration_date,
     },
   });
 });
 
-app.get("/dashboard/vehicles_toytags", requireAuth, (req, res) => {
+app.get("/dashboard/vehicles_toytags", requireAuth, async (req, res) => {
+  const session = await auth.api.getSession({
+    headers: fromNodeHeaders(req.headers),
+  });
+  const vehiclesDb = testDb.collection("vehicles");
+  const toytagsDb = testDb.collection("toytags");
+
+  const vehiclesCount: number = await vehiclesDb.countDocuments({
+    account_id: session?.user.id,
+  });
+  const toytagsCount: number = await toytagsDb.countDocuments({
+    account_id: session?.user.id,
+  });
+
   res.send({
-    vehicles: 1,
-    toytags: 1,
+    vehicles: vehiclesCount,
+    toytags: toytagsCount,
   });
 });
 
@@ -55,6 +82,40 @@ app.get("/dashboard/activity", requireAuth, (req, res) => {
     payments: 1,
     adjustments: 0,
     refunds: 0,
+  });
+});
+
+///////////////////////////////////////////////////////////////////////////////
+// Vehicles & TOYTAGs Routes //////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+app.get("/vehicles", requireAuth, async (req, res) => {
+  const session = await auth.api.getSession({
+    headers: fromNodeHeaders(req.headers),
+  });
+  const vehiclesDb = testDb.collection("vehicles");
+
+  const vehiclesData = await vehiclesDb
+    .find({ account_id: session?.user.id })
+    .toArray();
+
+  res.send({
+    vehicles: vehiclesData,
+  });
+});
+
+app.get("/vehicles/toytags", requireAuth, async (req, res) => {
+  const session = await auth.api.getSession({
+    headers: fromNodeHeaders(req.headers),
+  });
+  const toytagsDb = testDb.collection("toytags_vehicles");
+
+  const toytagsData = await toytagsDb
+    .find({ account_id: session?.user.id })
+    .toArray();
+
+  res.send({
+    toytags: toytagsData,
   });
 });
 
